f3f3b850e99700f9ac0a6a2c6d4f1c9bcc644fae
filip97155 <lipeqo@gmail.com>
2026-04-12 21:04:45 +0200

    Fix backend test project and exclude Tests from main build

--- backend/Tests/obj/Debug/net9.0/CloudBackend.Tests.AssemblyInfo.cs
+++ backend/Tests/obj/Debug/net9.0/CloudBackend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudBackend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e46c848c9c509f222f58140890f1bf7b77dfcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudBackend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudBackend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
